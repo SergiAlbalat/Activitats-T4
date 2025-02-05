71ab5bdee954353aba9efcb66395b037c17e989f
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-02-05 12:06:18 +0100

    Exercici 11 Completat Closes #20

--- Program.cs
+++ Program.cs
@@ -7,18 +7,9 @@ namespace Activitats_T4
     {
         public static void Main()
         {
-            const string MsgInstructions = "Enter two dates in DD-MM-YYYY and i will tell you if the second one is bigger tha the first one";
-            const string MsgBigger = "The second date is bigger";
-            const string MsgSmaller = "The second date is smaller";
-            int day, month;
-            Console.WriteLine(MsgInstructions);
-            day = MyMethods.GetNumber();
-            month = MyMethods.GetNumber();
-            DateTime firstDate = new DateTime(MyMethods.GetNumber(), month, day);
-            day = MyMethods.GetNumber();
-            month = MyMethods.GetNumber();
-            DateTime secondDate = new DateTime(MyMethods.GetNumber(), month, day);
-            Console.WriteLine(DateTime.Compare(firstDate, secondDate) > 0 ? MsgSmaller : MsgBigger);
+            DateTime dateTime = DateTime.Now;
+            DayOfWeek dayOfWeek = dateTime.DayOfWeek;
+            Console.WriteLine(dayOfWeek.ToString());
         }
     }
 }
