5b6db0f9d2b929f598c4ef854dee677d35833b7e
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-02-05 11:52:09 +0100

    Exercici 10 completat Closes #19

--- Program.cs
+++ Program.cs
@@ -7,39 +7,18 @@ namespace Activitats_T4
     {
         public static void Main()
         {
-            const string MsgSearchName = "What name do you want to search in the dictionary?";
-            const string MsgNameFound = "It's in the dictionary!";
-            const string MsgNameNotFound = "It's not in the dictionary!";
-            const string MsgNageAge = "The age of {0} is: {1}";
-            const string MsgFormatError = "The format of the name is incorrect";
-            string searchKey;
-            Dictionary<string, int> studentAges = new Dictionary<string, int>();
-            studentAges.Add("Marc", 21);
-            studentAges.Add("Laura", 19);
-            studentAges.Add("Pau", 22);
-            foreach(var i in studentAges)
-            {
-                Console.WriteLine("{0}: {1}", i.Key, i.Value);
-            }
-            Console.WriteLine(MsgSearchName);
-            try
-            {
-                searchKey = Console.ReadLine();
-                Console.WriteLine(studentAges.ContainsKey(searchKey));
-                if (studentAges.ContainsKey(searchKey)){
-                    Console.WriteLine(MsgNageAge, searchKey, studentAges[searchKey]);
-                }
-                studentAges.Remove("Laura");
-                foreach (var i in studentAges)
-                {
-                    Console.WriteLine("{0}: {1}", i.Key, i.Value);
-                }
-            }
-            catch(FormatException)
-            {
-                Console.WriteLine(MsgFormatError);
-            }
-            
+            const string MsgInstructions = "Enter two dates in DD-MM-YYYY and i will tell you if the second one is bigger tha the first one";
+            const string MsgBigger = "The second date is bigger";
+            const string MsgSmaller = "The second date is smaller";
+            int day, month;
+            Console.WriteLine(MsgInstructions);
+            day = MyMethods.GetNumber();
+            month = MyMethods.GetNumber();
+            DateTime firstDate = new DateTime(MyMethods.GetNumber(), month, day);
+            day = MyMethods.GetNumber();
+            month = MyMethods.GetNumber();
+            DateTime secondDate = new DateTime(MyMethods.GetNumber(), month, day);
+            Console.WriteLine(DateTime.Compare(firstDate, secondDate) > 0 ? MsgSmaller : MsgBigger);
         }
     }
 }
