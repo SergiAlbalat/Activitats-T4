4f9aa93235cd627bf893bfdc0c0adf8d0dc854e9
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-02-06 13:59:23 +0100

    Exercici 12 Acabat Closes #21

---
+++ Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Activitats_T4
+{
+    public class Employee
+    {
+        public int CalculateAge(DateTime fechaNacimiento)
+        {
+            TimeSpan daysLived = -(fechaNacimiento.Subtract(DateTime.Now));
+            int age = daysLived.Days / 365;
+            return age;
+        }
+    }
+}
