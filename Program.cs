917b52827475caa35c207db7a9a9841c636bbcb7
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-02-13 12:35:37 +0100

    Exercici18 acabat Closes #32

--- Program.cs
+++ Program.cs
@@ -7,24 +7,22 @@ namespace Activitats_T4
     {
         public static void Main()
         {
-            List<Employee> employeeList = new List<Employee>() {
-                new Employee("0000", "Marcos", "Barrero", new DateTime(2006, 03, 04), 1301.01f, 30),
-                new Employee(),
-                new Employee("0001", "Ruben", "Borrego", new DateTime(2006, 07, 02)),
-                new Employee("0002", "Gabri", "De Souza", new DateTime(1939, 04, 01), 0.01f, 1),
-                new Employee("0003", "Elon", "Musk", DateTime.Now)
-            };
-            List<SalesEmployee> salesEmployeeList = new List<SalesEmployee>() {
-                new SalesEmployee("0010", "Leon", "Kennedy", new DateTime(2006, 03, 04), 10301.01f, 30, 2.0f),
-                new SalesEmployee(),
-                new SalesEmployee("0011", "Gabro", "Chill", new DateTime(2006, 07, 02), 1000f, 24, 1.0f),
-                new SalesEmployee("0012", "Cabri", "De Mostaza", new DateTime(1939, 04, 01), 0.01f, 1, 0.10f),
-                new SalesEmployee("0013", "Elun", "Mosk", DateTime.Now, 1000000.00f, 1, 0.01f)
-            };
-            employeeList = employeeList.OrderBy(x => x.HireDate).ToList();
-            salesEmployeeList = salesEmployeeList.OrderBy(z => z.HireDate).ToList();
-            employeeList.ForEach(x => Console.WriteLine(x));
-            salesEmployeeList.ForEach(x => Console.WriteLine(x));
+            const string MsgInstructions = "Write a number and i will remove from the list all the numbers above it\nList:";
+            const string MsgResult = "The final list is:";
+            int num;
+            Console.WriteLine(MsgInstructions);
+            List<int> numList = new List<int>() { 3, 12, 26, 33, 40, 43, 56 };
+            numList.ForEach(num => Console.WriteLine(num));
+            try
+            {
+                num = MyMethods.GetNumber();
+                numList = numList.Where(n => n < num).ToList();
+                Console.WriteLine(MsgResult);
+                numList.ForEach(num => Console.WriteLine(num));
+            }catch(FormatException error)
+            {
+                Console.WriteLine(error.Message);
+            }
         }
     }
 }
